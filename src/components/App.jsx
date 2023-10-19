985c78cf90321297bbea6e949150ec081808f951
Vpsush <vpsush2023@gmail.com>
2023-10-19 21:49:06 +0300

    11

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+
+export class App extends Comment {
+  state = {
+    contacts: [],
+    name: '',
+  };
+  render() {
+    return (
+      <div
+      // style={{
+      //   height: '100vh',
+      //   display: 'flex',
+      //   justifyContent: 'center',
+      //   alignItems: 'center',
+      //   fontSize: 40,
+      //   color: '#010101',
+      // }}
+      >
+        React homework template
+      </div>
+    );
+  }
+}
